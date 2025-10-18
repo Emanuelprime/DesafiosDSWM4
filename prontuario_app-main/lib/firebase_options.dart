9e57c3a632bb117b7879fcbedfe09267423facc0
Emanuelprime <141081170+Emanuelprime@users.noreply.github.com>
2025-10-18 18:04:43 -0300

    arrumando-firebase

--- prontuario_app-main/lib/firebase_options.dart
+++ prontuario_app-main/lib/firebase_options.dart
@@ -1,15 +1,13 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
-import 'package:flutter/foundation.dart'
-    show defaultTargetPlatform, kIsWeb, TargetPlatform;
 
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     return const FirebaseOptions(
-      apiKey: 'AIzaSyArf87H82RRYponnqIJz6QFlGtm4EoO6Es',
-      appId: '1:246137444053:android:a18bb9f128d9a29792745c',
+      apiKey: 'AIzaSyBpOBH4ZK3DLDrYZN4_YnQB3udhMbu7mSo',
+      appId: '1:1043374439093:android:643a009d3555b2fa3a5cfc',
       messagingSenderId: '246137444053',
-      projectId: 'prontuario-b0e08',
-      storageBucket: 'prontuario-b0e08.firebasestorage.app',
+      projectId: 'prontuariodesafio',
+      storageBucket: 'prontuariodesafio.firebasestorage.app',
     );
   }
 }
